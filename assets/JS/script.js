d35c6057845d29eddfaa14ef865eb0e523159c90
Jeruelrichard <okemdinach383@gmail.com>
2026-01-27 17:27:01 +0100

    testing scroll animation

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -18,3 +18,38 @@ const disableDarkmode =() => {
     document.body.classList.remove('darkmode')
     localStorage.setItem('darkmode', null)
 }
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+  sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.navLinks a[href*=' + sectionId + ']')
+        
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('activeLink')
+        }else{
+            sectionsClass.classList.remove('activeLink')
+        }                                                    
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+
+ sr.reveal('.homeDeets, .abtDeets, .skillsCards, .card',{}); 
+sr.reveal('.homeImg, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.homeSocials',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
